fdeb85c3b8a17ccf5750f7fab36d2f389390ba4b
Alejo-Rey <alejo.rey.128@gmail.com>
2025-02-17 14:32:45 -0500

    Change: - Request changed to Faraday Update: - Products can be created multiple at once

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
 
-    render json: @product, status: 200
+    render json: @product, status: :ok
 
   rescue ActiveRecord::RecordNotFound
     render json: { error: "Product not found" }, status: :not_found
@@ -18,7 +18,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
 
     if @product.update!(product_params)
-      render json: @product, status: 200
+      render json: @product, status: :ok
     else
       render json: { errors: @product.errors }, status: :unprocessable_entity
     end
@@ -28,18 +28,39 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new(product_params)
+    products_data = product_params
 
-    if @product.save
-      render json: @product, status: :created
-    else
-      render json: @product.errors, status: :unprocessable_entity
+    if products_data.blank?
+      return render json: { error: "No products provided" }, status: :unprocessable_entity
     end
+
+    created_products = []
+
+    ActiveRecord::Base.transaction do
+      products_data.each do |product_data|
+        product = Product.new(product_data)
+        if product.save
+          created_products << product
+        else
+          raise ActiveRecord::Rollback, "Error: #{product.errors.full_messages.join(', ')}"
+        end
+      end
+    end
+
+    render json: created_products, status: :created
+  rescue => e
+    render json: { error: e.message }, status: :unprocessable_entity
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :quantity)
+    permitted_params = params.permit(products: [ :name, :description, :price, :quantity ]).fetch(:products, [])
+
+    if permitted_params.is_a? Array
+      return permitted_params
+    end
+
+    params.require(:products).permit(:name, :description, :price, :quantity)
   end
 end
