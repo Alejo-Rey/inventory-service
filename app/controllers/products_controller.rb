198525933e5e4f839cbae31f171d31dbbbb3ca69
Alejo-Rey <alejo.rey.128@gmail.com>
2025-02-18 15:37:48 -0500

    Change: - Orders now update the quantity of the products once created. - move controller logic to services Update: - products can be updated multiple at once Fix: - rubocop offenses - access debugger in docker console

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 class ProductsController < ApplicationController
-
   def index
     @products = Product.all
     render json: @products
@@ -25,6 +24,32 @@ class ProductsController < ApplicationController
 
   rescue ActiveRecord::RecordNotFound
     render json: { error: "Product not found" }, status: :not_found
+  end
+
+  def bulk_update
+    products_data = product_params
+
+    if products_data.blank?
+      return render json: { error: "No products provided" }, status: :unprocessable_entity
+    end
+
+    updated_products = []
+
+    ActiveRecord::Base.transaction do
+      products_data.each do |product_data|
+        product = Product.find(product_data[:product_id])
+
+        if product.update!(quantity: product[:quantity].to_i - product_data[:quantity].to_i)
+          updated_products << product
+        else
+          raise ActiveRecord::Rollback, "Error: #{product.errors.full_messages.join(', ')}"
+        end
+      end
+    end
+
+    render json: updated_products, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Products not found" }, status: :not_found
   end
 
   def create
@@ -55,7 +80,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    permitted_params = params.permit(products: [ :name, :description, :price, :quantity ]).fetch(:products, [])
+    permitted_params = params.permit(products: [ :name, :description, :price, :product_id, :quantity ]).fetch(:products, [])
 
     if permitted_params.is_a? Array
       return permitted_params
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
-
   # Validations
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
+
+  validates :quantity, presence: true, numericality: { greater_than: 0 }, if: :new_record?
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }, unless: :new_record?
 end
